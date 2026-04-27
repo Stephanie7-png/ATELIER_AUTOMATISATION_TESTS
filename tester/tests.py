4022d9da9403546eab4dfcc3dd4f9dcf0a90ab3b
Stephanie7-png <stephaniemakeu7@gmail.com>
2026-04-27 19:21:41 +0200

    Refactor tests to check for 'ip' field and latency

--- tester/tests.py
+++ tester/tests.py
@@ -1,7 +1,7 @@
 from tester.client import get_json
 
 def test_status_200():
-    res = get_json("/get/demo/key")
+    res = get_json("/?format=json")
     return {
         "name": "HTTP status 200",
         "success": res["status_code"] == 200,
@@ -9,44 +9,48 @@ def test_status_200():
     }
 
 def test_content_type_json():
-    res = get_json("/get/demo/key")
+    res = get_json("/?format=json")
     return {
         "name": "Content-Type JSON",
         "success": "application/json" in res["content_type"],
         "details": res
     }
 
-def test_field_value_exists():
-    res = get_json("/get/demo/key")
+def test_field_ip_exists():
+    res = get_json("/?format=json")
     data = res["json"]
+
     return {
-        "name": "Champ obligatoire 'value' présent",
-        "success": isinstance(data, dict) and "value" in data,
+        "name": "Champ ip présent",
+        "success": isinstance(data, dict) and "ip" in data,
         "details": res
     }
 
-def test_value_is_integer():
-    res = get_json("/get/demo/key")
+def test_ip_is_string():
+    res = get_json("/?format=json")
     data = res["json"]
+
     return {
-        "name": "Le champ 'value' est un entier",
-        "success": isinstance(data, dict) and isinstance(data.get("value"), int),
+        "name": "Champ ip est string",
+        "success": isinstance(data, dict) and isinstance(data.get("ip"), str),
         "details": res
     }
 
-def test_invalid_endpoint_404():
-    res = get_json("/fake/demo/key")
-    return {
-        "name": "Endpoint invalide retourne 404",
-        "success": res["status_code"] == 404,
-        "details": res
-    }
+def test_latency_under_3s():
+    res = get_json("/?format=json")
 
-def test_latency_under_3_seconds():
-    res = get_json("/get/demo/key")
     return {
-        "name": "Latence inférieure à 3 secondes",
+        "name": "Latence < 3 sec",
         "success": res["latency_ms"] is not None and res["latency_ms"] < 3000,
+        "details": res
+    }
+
+def test_invalid_endpoint():
+    res = get_json("/fake")
+
+    return {
+        "name": "Endpoint invalide",
+        "success": res["status_code"] in [400, 404],
         "details": res
     }
 
@@ -54,8 +58,8 @@ def run_all_tests():
     return [
         test_status_200(),
         test_content_type_json(),
-        test_field_value_exists(),
-        test_value_is_integer(),
-        test_invalid_endpoint_404(),
-        test_latency_under_3_seconds()
+        test_field_ip_exists(),
+        test_ip_is_string(),
+        test_latency_under_3s(),
+        test_invalid_endpoint()
     ]
